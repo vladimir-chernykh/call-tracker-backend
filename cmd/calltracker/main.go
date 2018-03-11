6179bf42fe44fb80379ec903f079d67e4d3136ec
Max Ryndin <rgmasim@gmail.com>
2018-03-11 22:25:44 +0300

    Add stub.

--- cmd/calltracker/main.go
+++ cmd/calltracker/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"net/http"
+	"fmt"
+	"github.com/gorilla/mux"
+)
+
 func main() {
-	
+	run()
+}
+
+func run() {
+	r := mux.NewRouter()
+
+	r.Handle("/api/v1/phones/{phone}", stubUploadAudioHandler())
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func stubUploadAudioHandler() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, rr *http.Request) {
+		vars := mux.Vars(rr)
+		if vars["phone"] == "+79161298967" {
+			rw.WriteHeader(http.StatusUnprocessableEntity)
+		} else {
+			rw.WriteHeader(http.StatusCreated)
+		}
+
+	})
 }
