9834ad653519a94cb44d97948701fa2af091ac44
Max Ryndin <rgmasim@gmail.com>
2018-03-21 22:18:09 +0300

    Make it pretty.

--- db/run.sh
+++ db/run.sh
@@ -3,4 +3,4 @@
 docker volume create call_tracker_pgdata
 # everytime
 docker build -t call-tracker-db .
-docker run -p 5432:5432 -d -v call_tracker_pgdata:/var/lib/postgresql/data --name call_tracker_db call-tracker-db
+docker run -p 5432:5432 -d -v call_tracker_pgdata:/var/lib/postgresql/data --name call-tracker-db masim05/call-tracker-db
