2286e14b7f6db387c1b27dea809f3ace3e7c5358
Max Ryndin <rgmasim@gmail.com>
2018-04-07 12:10:38 +0300

    Add build commands.

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 docker build -t call-tracker-backend .
+# docker tag 5b3ff735943a masim05/call-tracker-backend
+# docker push masim05/call-tracker-backend
 docker run -p 80:80 -d --name call-tracker-backend masim05/call-tracker-backend
